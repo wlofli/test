8311f8607d4f8e8375e05d5571c3367f6c8af55b
wlofli <378476772@qq.com>
2015-07-13 15:11:55 +0800

    ..
    ..

---
+++ src/base/wolf/model/BaseModel.java
@@ -0,0 +1,18 @@
+package base.wolf.model;
+
+import java.util.Date;
+
+/**
+ * author lzc
+ * 2015年7月4日下午1:47:07
+ */
+public class BaseModel {
+	private String id;
+	private String deleted;
+	private String modifiedId;
+	private Date modiiedTime;
+	private String createdId;
+	private Date createdTime;
+	
+	
+}
---
+++ src/base/wolf/struct/Tree.java
@@ -0,0 +1,11 @@
+package base.wolf.struct;
+/**
+ * author lzc
+ * 2015年7月2日上午9:10:20
+ */
+public class Tree {
+	private String name;
+	private String pid;
+	
+
+}
