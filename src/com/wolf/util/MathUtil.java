832d28c9f518aea49ff17d6056af215da3d8cc18
wlofli <378476772@qq.com>
2015-07-14 11:07:23 +0800

    ..
    ..

--- src/com/wolf/util/MathUtil.java
+++ src/com/wolf/util/MathUtil.java
@@ -6,6 +6,15 @@ import java.util.List;
 
 
 public  class MathUtil {
+	
+	/**
+	 * 
+	 * add by lzc
+	 * date: 2015年7月14日
+	 * @param sum 分母
+	 * @param suc 分子
+	 * @return
+	 */
 	public static float getPercent(int sum, int suc){
 		if(sum == 0 || suc == 0){
 			return 0;
