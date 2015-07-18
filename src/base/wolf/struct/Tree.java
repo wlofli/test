54bbbd964493e21b2e03a79cb857a333c7c4fd1c
wlofli <378476772@qq.com>
2015-07-18 15:15:02 +0800

    ..
    ..

--- src/base/wolf/struct/Tree.java
+++ src/base/wolf/struct/Tree.java
@@ -1,9 +1,17 @@
 package base.wolf.struct;
 /**
  * author lzc
- * 2015年7月2日上午9:10:20
+ * 2015骞�7鏈�2鏃ヤ笂鍗�9:10:20
+ * undone
+ * 中文字问题未发现
+ */
+/**
+ * 
+ * @author lzc
+ * @date 2015年7月18日下午3:13:38
  */
 public class Tree {
+	private String id;  
 	private String name;
 	private String pid;
 	
