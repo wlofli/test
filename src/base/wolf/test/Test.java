23c46ba16c5c2b6ce4f265dc073e8fdc24438ae3
wlofli <378476772@qq.com>
2015-07-23 14:54:32 +0800

    ..
    ..

---
+++ src/base/wolf/test/Test.java
@@ -0,0 +1,21 @@
+package base.wolf.test;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import com.sun.org.apache.bcel.internal.generic.NEW;
+
+/**
+ * author lzc2015年7月23日上午11:27:52
+ */
+public class Test {
+
+	
+	public static void main(String[] args) {
+		SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+		System.currentTimeMillis();
+		Long time = Long.parseLong("1410243205000");
+		Date date = new Date(time);
+		System.out.println(simpleDateFormat.format(date));
+	}
+}
